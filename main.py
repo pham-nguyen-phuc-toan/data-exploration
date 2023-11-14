89ab86a25c5ead69b43ce178a0ed437586c9ab96
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2023-11-14 19:31:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,4 +11,6 @@ uploaded_file = st.file_uploader("Choose a .csv file", type=(['csv']))
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
+
+    st.header('Hiển thị dữ liệu')
     st.dataframe(df)
