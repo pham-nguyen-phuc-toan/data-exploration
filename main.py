a9b478b51e362cabf1b75ec15df72f60be34663e
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2023-11-14 22:14:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ if uploaded_file is not None:
     st.header('Show correlation between variables')
     st.table(df.corr())
 
-    df.hist(bins=20, figsize=(20, 20))
+    st.pyplot(df.hist(bins=20, figsize=(20, 20)))
