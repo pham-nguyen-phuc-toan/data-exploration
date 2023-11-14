6dca8445587fa674b5299d1d1713a8971c9c3c4b
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2023-11-14 19:22:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,27 +2,13 @@ import streamlit as st
 from PIL import Image
 import pickle as pkl
 import numpy as np
+import pandas as pd
 
-IMG_SIZE = 227
+st.title('Data exploration')
 
-class_list = {'0': 'NORMAL', '1': 'PNEUMONIA'}
-
-st.title('Pneumonia prediction based on chest X-Ray image')
-
-input = open('lrc_xray.pkl', 'rb')
-model = pkl.load(input)
-
-st.header('Upload chest X-Ray image')
-uploaded_file = st.file_uploader("Choose an image file", type=(['png', 'jpg', 'jpeg']))
+st.header('Upload a dataset')
+uploaded_file = st.file_uploader("Choose a .csv file", type=(['csv;]))
 
 if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Test image')
-
-    if st.button('Predict'):
-        image = image.resize((IMG_SIZE*IMG_SIZE*3, 1))
-        feature_vector = np.array(image)
-        label = str((model.predict(feature_vector))[0])
-
-        st.header('Result')
-        st.text(class_list[label])
+    df = pd.read_csv(uploaded_file)
+    st.dataframe(df)
